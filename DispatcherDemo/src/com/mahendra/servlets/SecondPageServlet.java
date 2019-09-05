e0d00be256a523e1be02b021e9d096f672486c6f
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-09-05 11:20:58 +0530

    Inter servlet communication

---
+++ DispatcherDemo/src/com/mahendra/servlets/SecondPageServlet.java
@@ -0,0 +1,49 @@
+package com.mahendra.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondPageServlet
+ */
+@WebServlet("/second.htm")
+public class SecondPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondPageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		out.println("Reading values from request scope");
+		String name = request.getAttribute("name").toString();
+		String job = request.getAttribute("job").toString();
+		out.println("Values fetched are name:"+name+", job:"+job);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
